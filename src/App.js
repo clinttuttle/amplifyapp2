23819534cdddb7f183c4da45b18142755b29ee37
Clint Tuttle <clinttuttle@users.noreply.github.com>
2021-11-06 01:54:08 -0500

    changes for v2

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Hello from V2</h1>
+    </header>
+  </div>
   );
 }
 
