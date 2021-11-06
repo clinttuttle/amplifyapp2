ee596fcc006ac808c6b647ec8609f8cd11d436f4
Clint Tuttle <clinttuttle@users.noreply.github.com>
2021-11-06 02:10:52 -0500

    changes for v7

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Updated this text!
+          Updated this text!! again!
         </p>
         <a
           className="App-link"
